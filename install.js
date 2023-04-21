245bbb768e2776546fa5a2e85ea66ee1c532e37b
Wheeler Law <wheeler.law@konghq.com>
2023-04-20 23:03:40 -0500

    Beginning work on uninstallation

--- install.js
+++ install.js
@@ -1,153 +1,91 @@
 'use strict';
 
-//
-// Compatibility with older node.js as path.exists got moved to `fs`.
-//
 const fs = require('fs')
-  , path = require('path')
-  , os = require('os')
-  , util = require('node:util')
-  , exec = util.promisify(require('node:child_process').exec)
+const path = require('path')
+const os = require('os')
+const util = require('node:util')
+const exec = util.promisify(require('node:child_process').exec)
 
-const {dot} = require("mocha/lib/reporters");
+// Find the highest filename in the series
+const findHighest = util.promisify(function recurse(dir, name, cb, num=1) {
+  let higherName = path.resolve(dir, `${name}${num <= 1 ? "" : `-${num}`}`)
+  return fs.promises.stat(higherName)
+    .then(_ => recurse(dir, name, cb, num+1))
+    .catch(_ => cb(undefined, higherName))
+})
+exports.findHighest = findHighest
 
-const gitFindHooksDirCommand = "git rev-parse --git-path hooks"
+const cwd = path.resolve(__dirname, "..")
+// const cwd = __dirname
+exports.cwd = cwd
 
-
-function manuallyGetGitFolderPath(currentPath) {
-  const dotGitPath = path.resolve(currentPath, ".git")
-  return fs.promises.stat(dotGitPath)
-    .then(stats => {
-      if (stats.isDirectory()) return dotGitPath
-      else {
-        console.log(`${dotGitPath} is a file, likely it's a submodule gitfile, searching it for .git directory location`)
-        return fs.promises.readFile(dotGitPath)
-          .then(file => /^gitdir: (.+)$/gm.exec(file.toString())[1])
-          .then(dotGitPath => {
-            dotGitPath = path.resolve(dotGitPath)
-            console.log(`Found ${dotGitPath} inside submodule gitfile`)
-            return dotGitPath
-          })
-          .catch(_ => "")
-      }
-    })
-    .catch(_ => "")
-    .then(potentialPath => {
-      if (potentialPath) return path.resolve(potentialPath)
-      else {
-        console.log(`Not found: ${dotGitPath}`)
-        const parentPath = path.resolve(currentPath, "..")
-        if (parentPath === currentPath) throw Error("Reached root and couldn't find .git dir")
-        else return manuallyGetGitFolderPath(parentPath)
-      }
-    })
-}
-
-async function main() {
-  const hooksDir = await exec(gitFindHooksDirCommand)
+const getGitHooksDir = async function(wd) {
+  const gitFindHooksDirCommand = "git rev-parse --git-path hooks"
+  return await exec(gitFindHooksDirCommand, {cwd: wd})
     .then(res => res.stdout.trim())
-    .then(path.resolve)
-    .catch(err => {
-      console.log("Couldn't get Git hooks directory location using Git:")
-      console.log(`> ${process.cwd()}$ ${gitFindHooksDirCommand}`)
-      console.log(err.stderr.trim().replace(/^/gm, `> `))
+    .then(dir => path.resolve(wd, dir))
+    .catch(err => console.log(`Something else went wrong: ${err.toString().trim()}`))
+}
+exports.getGitHooksDir = getGitHooksDir
 
-      console.log("Searching up the directory tree for .git...")
-      return manuallyGetGitFolderPath('.')
-        .then(dotGitPath => {
-          return exec(`${gitFindHooksDirCommand} --git-dir=${dotGitPath}`)
-            .then(res => res.stdout.trim())
-            .catch(err => {
-              console.log("Couldn't get Git hooks directory location using Git:")
-              console.log(`> ${dotGitPath}$ ${gitFindHooksDirCommand}`)
-              console.log(err.stderr.trim().replace(/^/gm, `> `))
-            })
-        })
-        .catch(err => console.log(`Error looking for .git dir: ${err.message}`))
-    })
-    .catch(err => console.log(`Something else went wrong: ${err}`));
+// Main function definition
+async function main() {
+  console.log(".")
+  console.log("Beginning installation")
 
+  const hooksDir = await getGitHooksDir(cwd)
 
   if (!hooksDir) {
     console.log("Could not locate git hooks directory")
     return
   }
 
-  console.log(`Found: ${hooksDir}`)
-  const preCommitHook = path.resolve(__dirname, "pre-commit")
-  const hookRelativeDir = path.relative(hooksDir, preCommitHook)
-  await fs.promises.mkdir(hooksDir)
-    .catch(err => {
-      let x = err
+  let hooksDirStats = await fs.promises.stat(hooksDir)
+    .then(stats => ({exists: stats.isDirectory(), message: stats.isDirectory() ? `Found: ${hooksDir}` : `Path ${hooksDir} is not a directory`}))
+    .catch(_ => {
+      return fs.promises.mkdir(hooksDir)
+        .then(_ => ({exists: true, message: `Created: ${hooksDir}`}))
+        .catch(err => ({exists: false, message: `Path ${hooksDir} could not be created: ${err}`}))
     })
 
+  console.log(hooksDirStats.message)
+  if (!hooksDirStats.exists) {
+    console.log("Quitting...")
+    return
+  }
+
+  let existingPreCommitHook = path.resolve(hooksDir, "pre-commit")
+  let movedExistingPreCommitHook = await fs.promises.stat(existingPreCommitHook)
+    .then(_ => findHighest(hooksDir, "pre-commit.old"))
+    .then(highestName => fs.promises.rename(existingPreCommitHook, highestName).then(_ => highestName))
+    .catch(_ => "")
+
+  if (movedExistingPreCommitHook) {
+    console.log(`Moved: ${existingPreCommitHook} -> ${movedExistingPreCommitHook}`)
+  }
+
+  const preCommitHook = path.resolve(__dirname, "hook")
+  let hookRelativeDir = path.relative(hooksDir, preCommitHook)
+  if(os.platform() === 'win32') {
+    hookRelativeDir = hookRelativeDir.replace(/[\\\/]+/g, '/');
+  }
+
+  const preCommitFileContents =
+    "#!/usr/bin/env bash\n" +
+    `# pre-commit hook for npm\n` +
+    `"${hookRelativeDir}"\n` +
+    `\n` +
+    `# original pre-commit hook\n` +
+    `${movedExistingPreCommitHook ? `"${path.relative(existingPreCommitHook, movedExistingPreCommitHook)}"\n` : ''}` +
+    `exit $?\n`
+
+  await fs.promises.writeFile(existingPreCommitHook, preCommitFileContents)
+    .then(_ => fs.promises.chmod(existingPreCommitHook, "755"))
+    .then(_ => console.log(`Created: ${existingPreCommitHook}`))
+    .catch(err => console.log(`Couldn't create hook: ${err}`))
+
+  console.log("Done!")
+  console.log(".")
 }
 
 main().then();
-return
-
-const hook = path.join(__dirname, 'hook')
-  , root = path.resolve(__dirname, '..', '..')
-
-
-let hooks = path.resolve(git, 'hooks')
-  , precommit = path.resolve(hooks, 'pre-commit');
-
-if (!exists(hooks)) fs.mkdirSync(hooks);
-
-//
-// If there's an existing `pre-commit` hook we want to back it up instead of
-// overriding it and losing it completely as it might contain something
-// important.
-//
-if (exists(precommit) && !fs.lstatSync(precommit).isSymbolicLink()) {
-  console.log('pre-commit:');
-  console.log('pre-commit: Detected an existing git pre-commit hook');
-  fs.writeFileSync(precommit +'.old', fs.readFileSync(precommit));
-  console.log('pre-commit: Old pre-commit hook backuped to pre-commit.old');
-  console.log('pre-commit:');
-}
-
-//
-// We cannot create a symlink over an existing file so make sure it's gone and
-// finish the installation process.
-//
-try { fs.unlinkSync(precommit); }
-catch (e) {}
-
-// Create generic precommit hook that launches this modules hook (as well
-// as stashing - unstashing the unstaged changes)
-// TODO: we could keep launching the old pre-commit scripts
-let hookRelativeUnixPath = hook.replace(root, '.');
-
-if(os.platform() === 'win32') {
-  hookRelativeUnixPath = hookRelativeUnixPath.replace(/[\\\/]+/g, '/');
-}
-
-const precommitContent = '#!/usr/bin/env bash' + os.EOL
-    + hookRelativeUnixPath + os.EOL
-    + 'RESULT=$?' + os.EOL
-    + '[ $RESULT -ne 0 ] && exit 1' + os.EOL
-    + 'exit 0' + os.EOL;
-
-//
-// It could be that we do not have rights to this folder which could cause the
-// installation of this module to completely fail. We should just output the
-// error instead destroying the whole npm install process.
-//
-try { fs.writeFileSync(precommit, precommitContent); }
-catch (e) {
-  console.error('pre-commit:');
-  console.error('pre-commit: Failed to create the hook file in your .git/hooks folder because:');
-  console.error('pre-commit: '+ e.message);
-  console.error('pre-commit: The hook was not installed.');
-  console.error('pre-commit:');
-}
-
-try { fs.chmodSync(precommit, '777'); }
-catch (e) {
-  console.error('pre-commit:');
-  console.error('pre-commit: chmod 0777 the pre-commit file in your .git/hooks folder because:');
-  console.error('pre-commit: '+ e.message);
-  console.error('pre-commit:');
-}
--- uninstall.js
+++ uninstall.js
@@ -1,10 +1,30 @@
-'use strict';
+'use strict'
 
-var fs = require('fs')
-  , path = require('path')
-  , exists = fs.existsSync || path.existsSync
-  , root = path.resolve(__dirname, '..', '..')
-  , git = path.resolve(root, '.git');
+const fs = require('fs')
+const path = require('path')
+const exists = fs.existsSync || path.existsSync
+const root = path.resolve(__dirname, '..', '..')
+const git = path.resolve(root, '.git')
+
+const {findHighest, getGitHooksDir, cwd} = require('./install')
+
+async function main() {
+  console.log(".")
+  console.log("Beginning uninstallation")
+
+  let hooksDir = getGitHooksDir(cwd)
+  let existingPreCommitHook = path.resolve(hooksDir, "pre-commit")
+
+
+  let fileContents = await fs.promises.readFile(existingPreCommitHook)
+  let isOurFile = fileContents.cont
+
+  let movedExistingPreCommitHook = await fs.promises.stat(existingPreCommitHook)
+    .then(_ => findHighest(hooksDir, "pre-commit.old"))
+    .then(highestName => fs.promises.rename(existingPreCommitHook, highestName).then(_ => highestName))
+    .catch(_ => "")
+}
+main.then()
 
 //
 // Resolve git directory for submodules
@@ -12,31 +32,31 @@ var fs = require('fs')
 if (exists(git) && fs.lstatSync(git).isFile()) {
   var gitinfo = fs.readFileSync(git).toString()
     , gitdirmatch = /gitdir: (.+)/.exec(gitinfo)
-    , gitdir = gitdirmatch.length == 2 ? gitdirmatch[1] : null;
+    , gitdir = gitdirmatch.length == 2 ? gitdirmatch[1] : null
 
   if (gitdir !== null) {
-    git = path.resolve(root, gitdir);
+    git = path.resolve(root, gitdir)
   }
 }
 
 //
 // Location of pre-commit hook, if it exists
 //
-var precommit = path.resolve(git, 'hooks', 'pre-commit');
+var precommit = path.resolve(git, 'hooks', 'pre-commit')
 
 //
 // Bail out if we don't have pre-commit file, it might be removed manually.
 //
-if (!exists(precommit)) return;
+if (!exists(precommit)) return
 
 //
 // If we don't have an old file, we should just remove the pre-commit hook. But
 // if we do have an old precommit file we want to restore that.
 //
 if (!exists(precommit +'.old')) {
-  fs.unlinkSync(precommit);
+  fs.unlinkSync(precommit)
 } else {
-  fs.writeFileSync(precommit, fs.readFileSync(precommit +'.old'));
-  fs.chmodSync(precommit, '755');
-  fs.unlinkSync(precommit +'.old');
+  fs.writeFileSync(precommit, fs.readFileSync(precommit +'.old'))
+  fs.chmodSync(precommit, '755')
+  fs.unlinkSync(precommit +'.old')
 }
